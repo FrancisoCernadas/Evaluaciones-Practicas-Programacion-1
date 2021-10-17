2b96643d207fc4ab6fef1b6629f51c8fc740b291
FrancisoCernadas <92688943+FrancisoCernadas@users.noreply.github.com>
2021-10-17 15:07:20 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Primera_tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 6;
+            int c = 3;
+
+            Console.WriteLine("Los digitos de las operaciones son: ");
+            Console.WriteLine("a = 7  b = 6  c = 3");
+            Console.WriteLine(" ");
+            
+            //aacb
+            Console.WriteLine("El numero aacb es: ");
+            
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(" ");
+
+            //bca
+            Console.WriteLine("El numero bca es: ");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(a);
+            Console.WriteLine(" ");
+
+            //bcbc
+            Console.WriteLine("El numero bcbc es: ");
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.Write(c);
+        }
+    }
+}
